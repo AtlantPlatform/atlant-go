9163d568eafb3f5d096bbd0f8aeee3a0cf8599ec
vlatlant <vl@atlant.io>
2019-03-22 10:32:06 +0200

    integration tests: cluster of 3 nodes listening to each other

--- tests/110-cluster-of-3/run.sh
+++ tests/110-cluster-of-3/run.sh
@@ -2,6 +2,42 @@
 set -e
 cd "$(dirname "$0")"  && pwd
 
+cleanup() {
+  sudo docker-compose logs
+  echo "NODE1=$NODE1"
+  echo "NODE2=$NODE2"
+  echo "NODE3=$NODE3"
+  sudo docker-compose down
+  sudo docker network rm clusterof3 || true  
+}
+
+expected() {
+  if [ "$1" == "" ]; then
+    cleanup
+    echo "[CLUSTER TESTS FAILED]"
+    exit 1
+  fi
+}
+
+start_service() {
+  echo "[`date`] Starting $1"
+  sudo docker-compose up -d --build $1
+}
+
+get_id() { 
+    # port, resultvar
+    local port=$1
+    local __resultvar=$2
+    local url="http://localhost:$port/api/v1/ping"
+    echo "[`date`] Requesting URL $url"
+    
+    RESULT=`curl $url`
+    echo "[`date`] Received $2: $RESULT"
+    eval $__resultvar="$RESULT"
+    expected $RESULT
+}
+
+
 if ! [ "`which docker-compose`" ]; then
   echo 'Error: docker-compose must be installed' >&2
   exit 1
@@ -11,30 +47,24 @@ fi
 sudo docker network create --driver bridge clusterof3 || true
 
 # starting the server
-echo "[`date`] Starting Auth Server"
-sudo docker-compose up -d --build auth
-echo "[`date`] Starting Node 1"
-sudo docker-compose up -d --build node1
-sleep 4
-NODE1=`curl -q http://localhost:33001/api/v1/ping || ''`
-echo "[`date`] NODE1=$NODE1"
-# TODO: give node permissions to write
+start_service auth
+start_service node1
+sleep 15
+get_id 33001 NODE1
 
-echo "[`date`] Starting Node 2"
-sudo docker-compose up -d --build node2
-sleep 5
-NODE1=`curl -q http://localhost:33002/api/v1/ping || ''`
-echo "[`date`] NODE2=$NODE2"
+echo "[`date`] Giving Node 1 permission to write"
+curl http://localhost:33700/ -d "$NODE1:write,sync"
 
-echo "[`date`] Starting Node 3"
-sudo docker-compose up -d --build node3
-sleep 5
-NODE1=`curl -q http://localhost:33003/api/v1/ping || ''`
-echo "[`date`] NODE3=$NODE3"
+start_service node2
+start_service node3
 
-echo "[`date`] Waiting for 10 seconds"
+sleep 30
+get_id 33002 NODE2
+get_id 33003 NODE3
+
+# sudo docker-compose exec -T node1 ./atlant-lite --help
+echo "[`date`] Waiting for 10 seconds, collecting logs"
 sleep 10
 
-sudo docker-compose logs
-sudo docker-compose down
-sudo docker network rm clusterof3 || true
+cleanup
+echo "[`date`] Cluster of 3 nodes was successfully verified"
