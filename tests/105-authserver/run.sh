87da55252522b6eb0f9a0ea4011e88cdfa69b127
vlatlant <vl@atlant.io>
2019-04-09 20:22:42 +0300

    fixing auth server tests

--- tests/105-authserver/run.sh
+++ tests/105-authserver/run.sh
@@ -7,6 +7,11 @@ if ! [ "`which docker-compose`" ]; then
   exit 1
 fi
 
+cleanup() {
+  docker-compose logs
+  docker-compose stop && docker-compose rm -f 
+}
+
 # starting the server
 docker-compose up -d --build
 retVal=$?
@@ -17,26 +22,36 @@ if [ $retVal -ne 0 ]; then
 fi
 
 # testing positive case:
-# That node is up
-curl -q -X GET http://localhost:33700/
+# That node is up in 20 secs
+let COUNTER=4
+# before continuing, ensure NODE0 exists
+while [[ "$(curl -s -o /dev/null -w '%{http_code}' http://localhost:33700/)" != "200" ]]; do 
+  echo "[`date`] Waiting for auth server... ($COUNTER)"
+  COUNTER=$((COUNTER - 1))
+  if [ "$COUNTER" == "0" ]; then
+    cleanup
+    echo "[`date`] TIMEOUT ERROR. auth node NOT STARTED"
+    exit 1
+  fi
+  sleep 5
+done
 
 # That node can write and result is the same
 EXPECTED="14V21784834838489:write,sync"
 curl -q http://localhost:33700/ -d "$EXPECTED"
 
-RESULT=`curl -q -X GET http://localhost:33700/`
+RESULT=`curl -sq -X GET http://localhost:33700/`
 echo "RESULT1:'$RESULT'"
 if [ "$RESULT" != "$EXPECTED" ]; then
     echo "EXPECTED:'$EXPECTED'"
     exit 1
 fi
 echo "RESPONSE OK"
-
-EXPECT_FORBIDDEN=`curl -q -H"X-Forwarded-For: 8.8.8.8" http://localhost:33700/ -d "$EXPECTED"`
+EXPECT_FORBIDDEN=`curl -sq -H"X-Forwarded-For: 8.8.8.8" http://localhost:33700/ -d "$EXPECTED"`
 if [ "$EXPECT_FORBIDDEN" != "No permission to update" ]; then
     echo "ERROR: Expected to be forbidden because of external address"
     exit 1
 fi
 
-docker-compose logs
-docker-compose down
+cleanup
+echo "[`date`] AUTH NODE was successfully verified"
